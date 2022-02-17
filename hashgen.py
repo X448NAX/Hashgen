3ce86b5c4c67b4a605cd1807881864418ac41882
Xanspie <94863419+X448NAX@users.noreply.github.com>
2022-02-17 10:50:15 +0000

    Added SHA3-512 and Shake256.

--- hashgen.py
+++ hashgen.py
@@ -1,13 +1,14 @@
-from hashlib import blake2b
-from hashlib import sha512, sha256, sha384, sha3_384
+from hashlib import blake2b, sha512, sha256, sha384, sha3_384, sha3_512, shake_256
 import secrets
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(prog='hashgen',epilog='https://github.com/X448NAX/Hashgen',description='Generate a random blake2b, SHA3-384, SHA256, SHA384, or SHA512 hash using safe entropy from urandom.')
-parser.version = 'hashgen v1.0.0'
+parser = argparse.ArgumentParser(prog='hashgen',epilog='https://github.com/X448NAX/Hashgen',description='Generate a random blake2b, SHA3-384, SHA3-512, SHAKE256, SHA256, SHA384, or SHA512 hash using safe entropy from urandom.')
+parser.version = 'hashgen v1.0.1'
 parser.add_argument("--blake2b", "-b", help="hashes random entropy using blake2b", dest="blake2b", action="store_true")
 parser.add_argument("--sha3", "-3", help="hashes random entropy using sha3-384", dest="sha3", action="store_true")
+parser.add_argument("--sha3512", "-35", help="hashes random entropy using sha3-512", dest="sha3512", action="store_true")
+parser.add_argument("--shake256", "-shake", help="hashes random entropy using shake-256", dest="shake256", action="store_true")
 parser.add_argument("--sha256", "-256", help="hashes random entropy using sha256", dest="sha256", action="store_true")
 parser.add_argument("--sha384", "-384", help="hashes random entropy using sha384", dest="sha384", action="store_true")
 parser.add_argument("--sha512", "-512", help="hashes random entropy using sha512", dest="sha512", action="store_true")
@@ -23,6 +24,8 @@ sha256hash = sha256(entropy).hexdigest()
 sha384hash = sha384(entropy).hexdigest()
 sha512hash = sha512(entropy).hexdigest()
 sha3_384hash = sha3_384(entropy).hexdigest()
+sha3_512hash = sha3_512(entropy).hexdigest()
+shake256hash = shake_256(entropy).hexdigest(length=42)
 
 args = parser.parse_args()
 
@@ -36,3 +39,7 @@ if args.sha512:
 	print(sha512hash)
 if args.sha3:
 	print(sha3_384hash)
+if args.sha3512:
+	print(sha3_512hash)
+if args.shake256:
+	print(shake256hash)
